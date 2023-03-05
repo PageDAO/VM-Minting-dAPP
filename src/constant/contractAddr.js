a1b6c0ab6008fe8faf1fcc0c6d76fec91d29d0c6
ipfsnut <40326908+ipfsnut@users.noreply.github.com>
2023-03-05 17:10:38 -0600

    add case Optimistic Goerli

--- src/constant/contractAddr.js
+++ src/constant/contractAddr.js
@@ -40,6 +40,14 @@ switch (NET) {
         Membership721ContractAddr = '0x836f84E121643b90c6aFf9f4D84561b0Ac69423c'
         MarketplaceContractAddr = '0xCFA7cA9fD5d32A6E9104aC849b1094Ea8c71e294'
         break;
+        
+    case 'optimism':
+        ComunityContractAddr = '0x75fFAF8711Cf9E0B36eaCdEf407f396F7628e976'
+        UniftyContractAddr = '0xE7F967Ed990C4db5262b592A3e5B70e29Dd585c3'
+        MembershipContractAddr = ''
+        Membership721ContractAddr = '0x94A56cBd3bb49664f333c3B559F2bD8cb9Ae55a0'
+        MarketplaceContractAddr = '0xd24b3de085CFd8c54b94feAD08a7962D343E6DE0'
+        break;
 
     default:
         break;
